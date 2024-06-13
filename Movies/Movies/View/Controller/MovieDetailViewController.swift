58c20029c990cd43209dc3359b9cce4f281b8212
eocb@gft.com <eduardocabral95@gmail.com>
2024-06-13 17:50:11 -0300

    Starting Detail

---
+++ Movies/Movies/View/Controller/MovieDetailViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MovieDetailViewController.swift
+//  Movies
+//
+//  Created by Cabral Costa, Eduardo on 13/06/24.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    private let movie: Movie
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let overviewLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    init(movie: Movie) {
+        self.movie = movie
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        title = movie.title
+        
+        view.addSubview(titleLabel)
+        view.addSubview(overviewLabel)
+        
+        titleLabel.text = movie.title
+        overviewLabel.text = movie.overview
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            overviewLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            overviewLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            overviewLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+}
