f436c3b5d546bdc825d2ebc4544ee6cfb41b2556
EduardoCostaEXE <eduardocabral95@gmail.com>
2024-06-17 23:13:19 -0300

    Category

---
+++ Movies/Movies/Model/MovieCategory.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCategory.swift
+//  Movies
+//
+//  Created by Cabral Costa, Eduardo on 17/06/24.
+//
+
+import Foundation
+
+enum MovieCategory {
+    case popular
+    case action
+    case comedy
+}
