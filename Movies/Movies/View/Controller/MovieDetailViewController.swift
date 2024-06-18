f83a0881f49937c56afad5054989ca88ed33d787
EduardoCostaEXE <eduardocabral95@gmail.com>
2024-06-17 22:18:38 -0300

    Genres sart and fix name

--- Movies/Movies/View/Controller/MovieDetailViewController.swift
+++ Movies/Movies/View/Controller/MovieDetailViewController.swift
@@ -9,19 +9,19 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
     private let movie: Movie
-    
+
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         return scrollView
     }()
-    
+
     private let contentView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
+
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -29,22 +29,31 @@ class MovieDetailViewController: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-    
+
     private let gradientLayer: CAGradientLayer = {
         let layer = CAGradientLayer()
         layer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
         layer.locations = [0.1, 1.0]
         return layer
     }()
-    
+
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 36, weight: .bold)
         label.textColor = .white
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
+    private let genresLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
     private let overviewLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
@@ -53,37 +62,38 @@ class MovieDetailViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
     init(movie: Movie) {
         self.movie = movie
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         configureView()
         updateScrollViewContentSize()
     }
-    
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         gradientLayer.frame = imageView.bounds
     }
-    
+
     private func configureView() {
         titleLabel.text = movie.title
+        genresLabel.text = "Ação | Aventura" // Substituir pelos gêneros reais do filme que virão por API
         overviewLabel.text = "\t\(movie.overview)"
         if let posterPath = movie.posterPath {
             let urlString = "https://image.tmdb.org/t/p/w500\(posterPath)"
             if let url = URL(string: urlString) {
                 URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
                     guard let self = self, let data = data else { return }
-                    
+
                     DispatchQueue.main.async {
                         if let image = UIImage(data: data) {
                             self.imageView.image = image
@@ -103,38 +113,42 @@ class MovieDetailViewController: UIViewController {
         self.contentView.layoutIfNeeded()
         self.scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.contentView.frame.height)
     }
-    
+
     private func setupView() {
         view.backgroundColor = .black
-        
+
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
         contentView.addSubview(imageView)
         contentView.addSubview(titleLabel)
+        contentView.addSubview(genresLabel)
         contentView.addSubview(overviewLabel)
-        
+
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: view.topAnchor),
             scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
+
             contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
             contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
             contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
             contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-            
+
             imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
             imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-//            imageView.heightAnchor.constraint(equalToConstant: 300),
             
             titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -200),
             titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            
-            overviewLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+
+            genresLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            genresLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            genresLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+
+            overviewLabel.topAnchor.constraint(equalTo: genresLabel.bottomAnchor, constant: 20),
             overviewLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             overviewLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             overviewLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
