4069b987feb366646809f1796ce301c144f9f2e1
EduardoCostaEXE <eduardocabral95@gmail.com>
2024-06-17 22:16:46 -0300

    Adjusting name in movie cell

--- Movies/Movies/View/Component/MovieCollectionViewCell.swift
+++ Movies/Movies/View/Component/MovieCollectionViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class MovieCollectionViewCell: UICollectionViewCell {
     static let identifier = "MovieCollectionViewCell"
-    
+
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -17,58 +17,53 @@ class MovieCollectionViewCell: UICollectionViewCell {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-    
+
     private let titleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
         label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
-    private let gradientLayer: CAGradientLayer = {
-        let layer = CAGradientLayer()
-        layer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
-        layer.locations = [0.7, 1.0]
-        return layer
-    }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupView() {
         contentView.addSubview(imageView)
         contentView.addSubview(titleLabel)
-        imageView.layer.addSublayer(gradientLayer)
-        
+
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
             imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            
-            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
-            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
-            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5)
+            imageView.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1.5),
+
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
         ])
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        gradientLayer.frame = imageView.bounds
-    }
-    
+
     func configure(with movie: Movie) {
         titleLabel.text = movie.title
         if let posterPath = movie.posterPath {
-            let urlString = "https://image.tmdb.org/t/p/w500\(posterPath)"
+            let urlString = "https://image.tmdb.org/t/p/w200\(posterPath)"
             if let url = URL(string: urlString) {
-                URLSession.shared.dataTask(with: url) { data, response, error in
-                    if let data = data {
-                        DispatchQueue.main.async {
-                            self.imageView.image = UIImage(data: data)
+                URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+                    guard let self = self, let data = data else { return }
+                    DispatchQueue.main.async {
+                        if let image = UIImage(data: data) {
+                            self.imageView.image = image
                         }
                     }
                 }.resume()
